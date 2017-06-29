12cb96ab7f24fb513b45720846e25f8997a6c04b
konojunya <junya03dance@gmail.com>
2017-06-29 13:53:38 +0900

    クリップボードへこぴー

--- js/script.js
+++ js/script.js
@@ -14,7 +14,8 @@
       contentType: false
     })
     .done(function(res){
-      console.log(res)
+      var downloadUrl = "http://makki0250.com:3000" + res.download_url
+      alert(downloadUrl)
     })
     .fail(function(jqXHR,textStatus,error){
       console.log("ERROR: ",jqXHR,textStatus,error)
@@ -23,5 +24,34 @@
     return false
 
   })
+
+  $(".copy").on("click",function(e){
+    var url = $(".downloadedElement .url").text()
+    if(execCopy(url)){
+      alert('コピーできました');
+    }
+    else {
+      alert('このブラウザでは対応していません');
+    }
+  })
+
+  function execCopy(string){
+    var temp = document.createElement('textarea');
+
+    temp.value = string;
+    temp.selectionStart = 0;
+    temp.selectionEnd = temp.value.length;
+
+    var s = temp.style;
+    s.position = 'fixed';
+    s.left = '-100%';
+
+    document.body.appendChild(temp);
+    temp.focus();
+    var result = document.execCommand('copy');
+    temp.blur();
+    document.body.removeChild(temp);
+    return result;
+  }
 
 })(jQuery)
